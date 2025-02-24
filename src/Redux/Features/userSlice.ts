37047b0889521edb7fbf04bc8c7be319a88f9b99
Fahmudul Hassan <fahmudul234@gmail.com>
2025-02-24 12:34:52 +0600

    feat
    initial setup

---
+++ src/Redux/Features/userSlice.ts
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+interface IUserState {
+  id?: string;
+  email?: string;
+  token?: string;
+  loggedInDeviceNumber?: number;
+}
+
+const initialState: IUserState = {};
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    
+  },
+});
---
+++ src/Routes/index.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <div>Hello from bank</div>,
+      },
+    ],
+  },
+]);
+export default router;
